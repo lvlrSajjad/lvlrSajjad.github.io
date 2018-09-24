7447c5394767ba56efee21b80b7be05f801f412a
Sajjad Asadi <lvlr.xaus@gmail.com>
2018-09-24 15:41:52 +0330

    total fix

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -327,7 +327,7 @@ jQuery(function ($) {
     // -------------------------------------------------------------
 
     (function () {
-        var myLatlng = new google.maps.LatLng(41.372641, -74.687387);
+        var myLatlng = new google.maps.LatLng(30.2839, 57.0834);
 
             var styles = [
                 {
